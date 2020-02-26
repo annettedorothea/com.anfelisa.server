9a9707212866c8b3d272c2344b03cd79ad75e8e1
Annette Pohl <annette_pohl@web.de>
2020-02-26 12:31:50 +0100

    wip get translation with deepL

--- src/main/java/com/anfelisa/card/actions/GetTranslationAction.java
+++ src/main/java/com/anfelisa/card/actions/GetTranslationAction.java
@@ -14,10 +14,12 @@
  * OR IN CONNECTION WITH THE USE OR PERFORMANCE OF THIS SOFTWARE.
  */
 
-
-
-
 package com.anfelisa.card.actions;
+
+import java.net.URL;
+import java.net.URLEncoder;
+import java.util.List;
+import java.util.Map;
 
 import org.jdbi.v3.core.Handle;
 import org.jdbi.v3.core.Jdbi;
@@ -28,31 +30,43 @@ import com.anfelisa.ace.CustomAppConfiguration;
 import com.anfelisa.ace.E2E;
 import com.anfelisa.ace.IDaoProvider;
 import com.anfelisa.ace.ViewProvider;
+import com.fasterxml.jackson.databind.ObjectMapper;
 
 public class GetTranslationAction extends AbstractGetTranslationAction {
 
 	static final Logger LOG = LoggerFactory.getLogger(GetTranslationAction.class);
 
-	public GetTranslationAction(Jdbi jdbi, CustomAppConfiguration appConfiguration, IDaoProvider daoProvider, 
+	public GetTranslationAction(Jdbi jdbi, CustomAppConfiguration appConfiguration, IDaoProvider daoProvider,
 			ViewProvider viewProvider, E2E e2e) {
-		super(jdbi,appConfiguration, daoProvider, viewProvider, e2e);
+		super(jdbi, appConfiguration, daoProvider, viewProvider, e2e);
 	}
 
-
+	@SuppressWarnings("unchecked")
 	protected void loadDataForGetRequest(Handle readonlyHandle) {
-		this.actionData.setTargetValue("XXX");
+		try {
+			String translationApiKey = appConfiguration.getTranslationApiKey();
+			String urlStr = "https://api.deepl.com/v2/translate?"
+					+ "auth_key=" + translationApiKey +
+					"&text=" + URLEncoder.encode(this.actionData.getSourceValue(), "UTF-8") +
+					"&source_lang=" + this.actionData.getSourceLanguage().toUpperCase() +
+					"&target_lang=" + this.actionData.getTargetLanguage().toUpperCase();
+			URL url = new URL(urlStr);
+			ObjectMapper mapper = new ObjectMapper();
+			Map<String, Object> map = mapper.readValue(url, Map.class);
+			List<Map<String, Object>> translations = (List<Map<String, Object>>) map.get("translations");
+			for (Map<String, Object> translation : translations) {
+				this.actionData.setTargetValue(translation.get("text").toString());
+			}
+		} catch (Exception e) {
+			//LOG.error(e.getMessage(), e);
+			this.actionData.setTargetValue("");
+			//App.reportException(e);
+		}
 	}
-	
+
 	public void initActionData() {
-		// init not replayable data here
 	}
 
 }
 
-
-
-
 /******* S.D.G. *******/
-
-
-
