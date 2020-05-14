d65b23afc512bbe4ba55e393b71d3a6a4161eaea
Annette Pohl <annette_pohl@web.de>
2020-05-14 14:47:05 +0200

    fixed assertThat method

--- src/test/java/de/acegen/BaseScenario.java
+++ src/test/java/de/acegen/BaseScenario.java
@@ -112,7 +112,7 @@ public abstract class BaseScenario extends AbstractBaseScenario {
 
 	@Override
 	protected void assertThat(int actual, int expected, String message) {
-		org.junit.Assert.assertEquals(message, is(expected), actual);
+		org.junit.Assert.assertEquals(message, expected, actual);
 	}
 	
 	@Override
